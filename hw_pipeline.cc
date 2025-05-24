0c1d13defb8e154c8ccd2e4e413431cc7a3d9851
proras <proras@outlook.com>
2025-05-24 16:19:39 +0800

    ENH: update taskflow processing with improved hardware submission and pipeline management

--- hw_pipeline.cc
+++ hw_pipeline.cc
@@ -15,32 +15,26 @@ const size_t num_lines = 128;
 void make_still_process_taskflow(tf::Taskflow& taskflow)
 {
     auto taskA = taskflow.emplace(
-        [](){ submit_to_hardware("NPU0", "NPU0", 20); },
-        [](){ submit_to_hardware("DSP0", "DSP0", 50); }
+        [](){ submit_to_hardware("NPU0", "post process1", 20); },
+        [](){ submit_to_hardware("DSP0", "post process2", 50); }
     );
 }
 
 void make_liveview_process_taskflow(tf::Taskflow& taskflow)
 {
     auto taskA = taskflow.emplace(
-        [](){ submit_to_hardware("NPU1", "NPU1", 1); },
-        [](){ submit_to_hardware("ISP0", "ISP0", 3); }
+        [](){ submit_to_hardware("NPU1", "ROI", 1); },
+        [](){ submit_to_hardware("ISP0", "HWPD", 3); }
     );
 }
 
-void make_coninue_capture_taskflow(tf::Taskflow& taskflow) {
-
-    auto still_process_executor = create_executor();
-    auto still_process_tf = std::make_shared<tf::Taskflow>();
-
+auto make_liveview_pipeline()
+{
     auto liveview_process_executor = create_executor();
-    auto liveview_process_tf = std::make_shared<tf::Taskflow>();
+    auto liveview_process_tf = create_taskflow();
 
     make_liveview_process_taskflow(*liveview_process_tf);
-
-    make_still_process_taskflow(*still_process_tf);
-
-    auto liveview_pipeline = new tf::Pipeline(num_lines,
+    auto *liveview_pipeline = new tf::Pipeline(num_lines,
         tf::Pipe{tf::PipeType::SERIAL, [](tf::Pipeflow& pipeflow) {
             if(pipeflow.token() == 6) {
                 pipeflow.stop();
@@ -50,38 +44,42 @@ void make_coninue_capture_taskflow(tf::Taskflow& taskflow) {
                 my_msleep(10);
                 return;
             }
-            submit_to_hardware("Sensor", "Sensor", 10);
+            submit_to_hardware("Sensor", "Liveview", 10);
         }},
         tf::Pipe{tf::PipeType::PARALLEL, [taskflow=liveview_process_tf, executor=liveview_process_executor](tf::Pipeflow& pipeflow) {
              executor->run(*taskflow);
         }}
     );
 
-    auto [still_frame_task, still_process_task] = taskflow.emplace(
+    return liveview_pipeline;
+}
+
+void make_coninue_capture_taskflow(tf::Taskflow& main_taskflow) {
+
+    auto still_process_executor = create_executor();
+    auto still_process_tf = create_taskflow();
+
+    make_still_process_taskflow(*still_process_tf);
+
+    auto liveview_pipeline = make_liveview_pipeline();
+
+    auto [still_frame_task, still_process_task] = main_taskflow.emplace(
         [liveview_pipeline](){ submit_to_hardware("Sensor", "Sensor", 110); liveview_pipeline->reset(); },
         [taskflow=still_process_tf, executor=still_process_executor](){ executor->run(*taskflow); }
     );
     still_frame_task.name("still_frame");
     still_process_task.name("still_process");
 
-    tf::Task liveview_task = taskflow.composed_of(*liveview_pipeline)
+    tf::Task liveview_task = main_taskflow.composed_of(*liveview_pipeline)
     .name("liveview pipeline");
 
     still_frame_task.precede(still_process_task, liveview_task);
-}
-
-void make_taskflow_spec(tf::Taskflow& taskflow, const std::string& hw_name,
-                        const std::string& node_name, int exec_time_ms) {
-    auto taskA = taskflow.emplace(
-        [hw_name, node_name, exec_time_ms](){ submit_to_hardware(hw_name, node_name, exec_time_ms); }
-    );
 }
 
 void tutorial_multi_pipeline()
 {
     tf::Taskflow taskflow("taskflow processing pipeline");
     tf::Executor executor;
-    tf::Executor executor_inner;
     const size_t num_pipes = 5;
 
     std::array<tf::Taskflow, num_pipes> taskflow_array;
@@ -94,7 +92,7 @@ void tutorial_multi_pipeline()
                 pipeflow.stop();
                 return;
             }
-            executor_inner.run(taskflow_array[pipeflow.pipe()]).wait();
+            executor.corun(taskflow_array[pipeflow.pipe()]);
         }}
     );
 
--- taskflow_helper.h
+++ taskflow_helper.h
@@ -1,18 +1,27 @@
 #pragma once
 
 #include <memory>
+#include <fstream>
 
 #include <queue>
 #include <taskflow/taskflow.hpp>
 #include <taskflow/algorithm/pipeline.hpp>
 
 inline std::queue<std::shared_ptr<tf::Executor>> g_executor_array;
+inline std::queue<std::shared_ptr<tf::Taskflow>> g_taskflow_array;
 
 inline std::shared_ptr<tf::Executor> create_executor()
 {
     auto executor = std::make_shared<tf::Executor>();
-    g_executor_array.push(executor);
+    g_executor_array.emplace(executor);
     return executor;
+}
+
+inline std::shared_ptr<tf::Taskflow> create_taskflow()
+{
+    auto taskflow = std::make_shared<tf::Taskflow>();
+    g_taskflow_array.emplace(taskflow);
+    return taskflow;
 }
 
 inline void wait_all_executors()
@@ -23,3 +32,8 @@ inline void wait_all_executors()
         executor->wait_for_all();
     }
 }
+
+ inline void dump_taskflow(tf::Taskflow& taskflow, const std::string& filename) {
+    std::ofstream taskflow_dumpfile(filename);
+    taskflow.dump(taskflow_dumpfile);
+ }
